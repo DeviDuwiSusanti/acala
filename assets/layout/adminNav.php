9c8afc3a7700315580ead328af285e64120a88f4
deviduwisusanti <125660222+DeviDuwiSusanti@users.noreply.github.com>
2025-01-25 12:16:00 +0700

    Add files via upload

---
+++ assets/layout/adminNav.php
@@ -0,0 +1,20 @@
+<div class="topbar">            
+    <div class="toggle">
+        <ion-icon name="menu-outline"></ion-icon>
+    </div>
+
+    <div class="user">
+        <img src="assets/img/logo.jpeg" alt="" >
+    </div>
+</div>
+
+<!-- <div class="container-fluid d-flex justify-content-between">
+        <ul class="navbar-nav ms-auto">
+            <li class="nav-item dropdown">
+                <img src="img/logo.jpeg" alt="" >
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div> -->
---
+++ assets/layout/adminSidebar.php
@@ -0,0 +1,68 @@
+<!-- =============== Navigation ================ -->
+<div class="container">
+        <div class="navigation">
+            <ul>
+                <li>
+                    <a href="Apendaptan.php">
+                        <span class="icon">
+                            <ion-icon name="logo-apple"></ion-icon>
+                        </span>
+                        <span class="title">Acala Bromo Admin</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="Apendapatan.php">
+                        <span class="icon">
+                            <ion-icon name="star-outline"></ion-icon>
+                        </span>
+                        <span class="title">Data Pendapatan</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="AreadUser.php">
+                        <span class="icon">
+                            <ion-icon name="people-outline"></ion-icon>
+                        </span>
+                        <span class="title">User</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="AreadBooking.php">
+                        <span class="icon">
+                            <ion-icon name="book-outline"></ion-icon>
+                        </span>
+                        <span class="title">Booking</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="AreadPaket.php">
+                        <span class="icon">
+                            <ion-icon name="bookmarks-outline"></ion-icon>
+                        </span>
+                        <span class="title">travel Package</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="AreadEvent.php">
+                        <span class="icon">
+                            <ion-icon name="star-outline"></ion-icon>
+                        </span>
+                        <span class="title">Event</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="logout.php">
+                        <span class="icon">
+                            <ion-icon name="star-outline"></ion-icon>
+                        </span>
+                        <span class="title">Logout</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
